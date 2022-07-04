0e2a502b5199251ad14c067c583ae25715c51f2a
eesehi <59656502+eesehi@users.noreply.github.com>
2022-07-05 03:55:04 +0530

    Controlled thread implementation

--- index.js
+++ index.js
@@ -1,38 +1,43 @@
 let input = document.querySelector('input')
-var cnt=0;
-function imageFinder(file){
-    cnt+=1;
-    if(cnt>5) return;
-    console.log(cnt);
-    Tesseract.recognize(
-        file,
-        'eng',
-        { logger: m => console.log(m+file.name) }
-      ).then(({ data: { text } }) => {
-        addRow(file.name,text)
-      })
-}
+var maxThread = (navigator.userAgent.search("Android")>-1)?3:4;
+var que = [];
 
-function addRow(name,text) {
-    var table = document.getElementById("myTable")
-    var row = table.insertRow(0)
-    var cell1 = row.insertCell(0)
-    var cell2 = row.insertCell(1)
-    cell1.innerHTML = name
-    cell2.innerHTML = text
-  }
+async function imageFinder() {
+        if(que.length == 0) return;
+        var file = que.pop();
+        Tesseract.recognize(file, "eng", {
+          logger: (m) => console.log(m + file.name),
+        }).then(({ data: { text } }) => {
+          addRow(file.name, text);
+          imageFinder();
+        });
+      }
+
+      function addRow(name, text) {
+        var table = document.getElementById("myTable");
+        var row = table.insertRow(0);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerHTML = name;
+        cell2.innerHTML = text;
+      }
 
 function get_ext(name) {
     return name.split('.').pop()
 }
 
-input.addEventListener('change',()=>{
-    let files=input.files
-    if(files.length==0) return
-    console.log(files)
-    for (let index = 0; index < files.length; index++) {
-        var file = files[index];
-        var ext=get_ext(file.name).toLowerCase()
-        if(ext=='jpeg' || ext=='jpg' || ext =='png' || ext=='bmp') imageFinder(file)
-    }
-})
+input.addEventListener("change", () => {
+        let files = input.files;
+        if (files.length == 0) return;
+        console.log(files);
+        for (let index = 0; index < files.length; index++) {
+          var file = files[index];
+          var ext = get_ext(file.name).toLowerCase();
+          if (ext == "jpeg" || ext == "jpg" || ext == "png" || ext == "bmp")
+            que.push(file);
+        }
+        for(var i=0;i<maxThread;i++){
+            imageFinder();
+        }
+        console.log("Haaloo");
+      });
