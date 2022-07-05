8605574320d0b3c53197e86afbecc7d194788131
eesehi <59656502+eesehi@users.noreply.github.com>
2022-07-05 05:49:23 +0530

    285% faster and removing unnecessary intialization of Tessaract (by reusing old threads)

--- index.js
+++ index.js
@@ -3,25 +3,26 @@ var maxThread = (navigator.userAgent.search("Android")>-1)?3:4;
 var que = [];
 
 async function imageFinder() {
-        if(que.length == 0) return;
-        var file = que.pop();
-        Tesseract.recognize(file, "eng", {
+        if (que.length == 0) return;
+
+        const worker = Tesseract.createWorker({
           logger: (m) => console.log(m),
-        }).then(({ data: { text } }) => {
-          addRow(file.name, text);
-          imageFinder();
         });
-      }
 
-      function addRow(name, text) {
-        var table = document.getElementById("myTable");
-        var row = table.insertRow(0);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        cell1.innerHTML = name;
-        cell2.innerHTML = text;
+        (async () => {
+          await worker.load();
+          await worker.loadLanguage("eng");
+          await worker.initialize("eng",0);
+          while (que.length != 0) {
+            var file = que.pop();
+            const {
+              data: { text },
+            } = await worker.recognize(file);
+            addRow(file.name, text);
+          }
+          await worker.terminate();
+        })();
       }
-
 function get_ext(name) {
     return name.split('.').pop()
 }
